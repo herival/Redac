6e5b72d71d5d01326a7a743c47d33ca4700e81d6
Herival <herimalala.val@gmail.com>
2023-10-04 14:12:38 +0200

    ok

--- src/Controller/Requetes/requetes.sql
+++ src/Controller/Requetes/requetes.sql
@@ -3,3 +3,30 @@ from inter i
 join user u
 where u.client = 'HELP1FO'
 and u.id = i.technicien_id;
+
+SELECT * FROM `saisie` WHERE `dateouverture` >= '2023-09-29 00:16:36' 
+LIKE `client` = 'SGNOVA' 
+AND 'prestation_id' = 5
+;
+
+SELECT * FROM `saisie` WHERE `dateouverture` >= '2023-09-29 00:16:36' AND `client` LIKE 'SGNOVA'
+
+SELECT * FROM `saisie` WHERE `prestation_id` = 5 
+AND `dateouverture` > '2023-09-29 00:16:36' 
+AND `dateouverture` < '2023-09-29 23:16:36' 
+AND `client` = 'SGNOVA'
+;
+
+# mise à jour saisie
+UPDATE `saisie` set `prestation_id` = 23  
+WHERE `prestation_id` = 5 
+AND `dateouverture` > '2023-09-29 00:16:36' 
+AND `dateouverture` < '2023-09-29 23:16:36' 
+AND `client` = 'SGNOVA'
+;
+
+SELECT * FROM `saisie` WHERE `prestation` = 'PREPARATION'
+AND `dateouverture` > '2023-09-29 00:16:36' 
+AND `dateouverture` < '2023-09-29 23:16:36' 
+AND `deposant` = 'SGNOVA'
+;
